aac70d549194f35052d3dc3382121b56c6ae9aa7
Istvan <17463906+purple0CaT@users.noreply.github.com>
2021-11-09 17:02:38 +0200

    Test works

--- src/app.js
+++ src/app.js
@@ -1,16 +1,20 @@
-import express from "express"
-import cors from "cors"
+import express from "express";
+import cors from "cors";
 import { productsRouter } from "./services/products/index.js";
+import { genericErrorHandler, mainErrorHandler } from "./errorHandler.js";
 
 const app = express();
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
-app.get('/test', (req, res) => {
-    res.send({ message: 'Test success' })
-})
+app.get("/test", (req, res) => {
+  res.send({ message: "Test success" });
+});
 
-app.use("/products", productsRouter)
+app.use("/products", productsRouter);
 
-export { app }
+app.use(mainErrorHandler);
+app.use(genericErrorHandler);
+
+export { app };
